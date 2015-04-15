996e95bc82862a2a8666385f065d0ad71456f7b7
Daniel Keler <danielk@jfrog.com>
2015-04-15 09:13:06 +0300

    Download/Upload - Artifactory/Bintray OK

---
+++ src/main/java/clientArtifactory.groovy
@@ -0,0 +1,44 @@
+/**
+ * Created by Dnaiel Keler.
+ */
+
+import org.apache.commons.io.IOUtils
+import org.jfrog.artifactory.client.ArtifactoryClient
+import org.jfrog.artifactory.client.model.File;
+
+class clientArtifactory {
+
+    static uploadFile_artifactory(String url, String user, String pass){
+        try {
+            File upAtrifact =  ArtifactoryClient
+                    .create(url, user, pass)
+                    .repository("ext-snapshot-local")
+                    .upload("test/artifactory-java-client-services-0.17.jar",
+                    (new FileInputStream("src/FileUpload/artifactory-java-client-services-0.17.jar")))
+                    .doUpload()
+        } catch (Exception gce){
+            println(gce.printStackTrace())
+        }
+    }
+
+    static downloadFile_artifactory(String url, String user, String pass){
+        try {
+            ByteArrayInputStream downAtrifact =  ArtifactoryClient
+                    .create(url, user, pass)
+                    .repository("ext-snapshot-local")
+                    .download("test/artifactory-java-client-services-0.17.jar")
+                    .doDownload()
+
+            IOUtils.copy(downAtrifact, new FileOutputStream('src/FileDownload/artifactory-java-client-services-0.17.jar'))
+
+        } catch (Exception gce){
+            println(gce.printStackTrace())
+        }
+    }
+
+    static void main (args) {
+//        uploadFile_artifactory("http://127.0.0.1:8081/artifactory", "admin", "password")
+        downloadFile_artifactory("http://127.0.0.1:8081/artifactory", "admin", "password")
+    }
+
+}
---
+++ src/main/java/clientBintray.groovy
@@ -0,0 +1,57 @@
+/**
+ * Created by Dnaiel Keler.
+ */
+
+
+import com.jfrog.bintray.client.impl.BintrayClient
+import org.apache.commons.io.IOUtils
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
+
+class clientBintray {
+
+    static uploadFile_bintray(){
+        Map connectionProperties = [
+                username:'danielk',
+                apikey:'ba7f6407fe4b3bd3dfdb3c98ecf28065d97cae6d',
+                email:'danielk@jfrog.com',
+                url:'https://api.bintray.net',
+                downloadUrl:''
+        ]
+
+        try {
+            def upload =  BintrayClient.create (
+                    connectionProperties.url as String,
+                    connectionProperties.username as String,
+                    connectionProperties.apikey as String )
+                    .subject('danielk')
+                    .repository('generic')
+                    .pkg('mypack')
+                    .version('1.0')
+                    .upload(
+                        "myfile/artifactory-java-client-services-0.17.jar",
+                    new FileInputStream("src/FileUpload/artifactory-java-client-services-0.17.jar"
+                    ))
+
+        } catch (Exception gce){
+//            FileNotFoundException
+            println(gce.printStackTrace())
+        }
+    }
+
+    static download_bintray (){
+        HttpGet req = new HttpGet('https://dl.bintray.net/danielk/generic/package-list')
+        HttpClient client = new DefaultHttpClient()
+        HttpResponse resp = client.execute(req)
+        InputStream file = resp.getEntity().getContent()
+        IOUtils.copy(file, new FileOutputStream('src/FileDownload/package-list'))
+    }
+
+    static void main (args) {
+//        uploadFile_bintray()
+        download_bintray()
+
+    }
+}
